9220d366fd4c5a862ec1fc488f83f37f8db8a82f
AshveerB <ashveerbhayroo@gmail.com>
2021-01-03 15:49:52 -0800

    add project card

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../footer/Footer';
+import ReactCardFlip from 'react-card-flip'
 
 
 const Projects = () => {
+    const [isFlipped, setIsFlipped] = useState(false)
+    const handleClick = () => {
+        setIsFlipped(!isFlipped)
+    }
     return (
         <div>
             Projects
+            <ReactCardFlip isFlipped={isFlipped} flipDirection='vertical'>
+                <div>
+                    <h2>Front</h2>
+                    <button className='front-flip-button' onClick={handleClick}>Flip</button>
+                </div>
+                <div>
+                    <h2>Back</h2>
+                    <button className='back-flip-button' onClick={handleClick}>Flip</button>
+                </div>
+            </ReactCardFlip>
             <div><Footer /></div>
         </div>
     );
--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Footer from '../footer/Footer';
+import { Document, Page } from 'react-pdf'
+import resume from '../images/Resume - Google Docs.pdf'
 
 
 const Resume = () => {
