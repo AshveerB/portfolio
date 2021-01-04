e5074815c325116c9f04220c7c9df9a0f2a4b03a
AshveerB <ashveerbhayroo@gmail.com>
2021-01-04 07:57:22 -0800

    complete project four card

--- src/components/projects/ProjectFour.js
+++ src/components/projects/ProjectFour.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
+import PMP from '../images/PMP - Imgur.png'
+import GitHubLogo from '../images/github.jpeg'
 
 const ProjectFour = () => {
     const [isFlipped, setIsFlipped] = useState(false)
@@ -10,13 +12,21 @@ const ProjectFour = () => {
         <div>
             <ReactCardFlip isFlipped={isFlipped} flipDirection='vertical'>
                 <div>
-                    <h2>Name</h2>
-                    <h2>Image</h2>
+                    <h2>Practice Makes Permanent</h2>
+                    <img src={PMP} alt='pmp'></img>
+                    <h2>
+                        React<br />Django<br />PostgreSQL<br />Postman<br />Python<br />Javascript
+                    </h2>
                     <button className='front-flip-button' onClick={handleClick}>Flip</button>
                 </div>
                 <div>
-                    <h2>Deployed Link</h2>
-                    <h2>Repo Link</h2>
+                    <a href='https://practice-makes-permanent-fe.herokuapp.com/' target='_blank' rel='noreferrer'>Deployed Site</a><br />
+                    <a href='https://github.com/AshveerB/practice-makes-permanent' target='_blank' rel='noreferrer'>Front End
+                    <img src={GitHubLogo} alt='gitHubLogo'></img>
+                    </a><br />
+                    <a href='https://github.com/AshveerB/practice-makes-permanent-back-end' target='_blank' rel='noreferrer'>Back End
+                    <img src={GitHubLogo} alt='gitHubLogo'></img>
+                    </a><br />
                     <button className='back-flip-button' onClick={handleClick}>Flip</button>
                 </div>
             </ReactCardFlip>
