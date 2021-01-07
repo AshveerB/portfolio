04108abbdb59cd8bd13d8554ae69f516f3df795f
AshveerB <ashveerbhayroo@gmail.com>
2021-01-06 18:40:50 -0800

    edit README.md

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import './Projects.css'
+import './Projects.css';
 import Footer from '../footer/Footer';
 import ProjectOne from './ProjectOne';
-import ProjectTwo from './ProjectTwo'
+import ProjectTwo from './ProjectTwo';
 import ProjectThree from './ProjectThree';
-import ProjectFour from './ProjectFour'
-import ProjectFive from './ProjectFive'
-
+import ProjectFour from './ProjectFour';
+import ProjectFive from './ProjectFive';
 
 const Projects = () => {
-    return (
-        <div>
-            Projects
-            <ProjectOne />
-            <ProjectTwo />
-            <ProjectThree />
-            <ProjectFour />
-            <ProjectFive />
-            <div><Footer /></div>
-        </div>
-    );
+	return (
+		<div>
+			Projects
+			<ProjectOne />
+			<ProjectTwo />
+			<ProjectThree />
+			<ProjectFour />
+			<ProjectFive />
+			<br />
+			<div>
+				<Footer />
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
