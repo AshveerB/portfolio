5a1ef548ce073409c0160a5905b65002d751387a
AshveerB <ashveerbhayroo@gmail.com>
2021-01-16 08:28:33 -0800

    add hover effect to navigation and footer elements

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,20 +4,35 @@ import './Footer.css'
 const Footer = () => {
     return (
 			<div className='footer'>
-				Contact Me: {' '}
+				Contact Me:{' '}
 				<a
 					href='https://www.linkedin.com/in/ashveer-bhayroo/'
 					target='_blank'
-					rel='noreferrer'>
+					rel='noreferrer'
+					className='hvr-grow'>
 					LinkedIn{' '}
 				</a>
-				<a href='https://github.com/AshveerB' target='_blank' rel='noreferrer'>
+				<a
+					href='https://github.com/AshveerB'
+					target='_blank'
+					rel='noreferrer'
+					className='hvr-grow'>
 					GitHub{' '}
 				</a>
-				<a href='https://codepen.io/ABhayroo' target='_blank' rel='noreferrer'>
+				<a
+					href='https://codepen.io/ABhayroo'
+					target='_blank'
+					rel='noreferrer'
+					className='hvr-grow'>
 					CodePen{' '}
 				</a>
-                <a href='https://docs.google.com/document/d/1i5VqLNqs5V4GaTSjt2dMuEdepHHzXmBbBoOFJNYRM9o/edit?usp=sharing' target='_blank' rel='noreferrer'>Resume</a>
+				<a
+					href='https://docs.google.com/document/d/1i5VqLNqs5V4GaTSjt2dMuEdepHHzXmBbBoOFJNYRM9o/edit?usp=sharing'
+					target='_blank'
+					rel='noreferrer'
+					className='hvr-grow'>
+					Resume
+				</a>
 			</div>
 		);
 };
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,10 +5,10 @@ import './Navigation.css'
 const Navigation = () => {
     return (
         <div>
-            <span><Link to='/home'>Home</Link></span>
-            <span><Link to='/about'>About</Link></span>
-            <span><Link to='/skills'>Skills</Link></span>
-            <span><Link to='/projects'>Projects</Link></span>
+            <span><Link to='/home' className='hvr-grow'>Home</Link></span>
+            <span><Link to='/about' className='hvr-grow'>About</Link></span>
+            <span><Link to='/skills' className='hvr-grow'>Skills</Link></span>
+            <span><Link to='/projects' className='hvr-grow'>Projects</Link></span>
         </div>
     );
 };
