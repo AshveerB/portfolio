0fd1cb31e11f63eb998e83a4fc9754d4d030d423
AshveerB <ashveerbhayroo@gmail.com>
2021-01-16 09:11:48 -0800

    edit spacing for home and about components

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <div>
             <h2 className='aboutTitle'>About</h2>
-            <p>I am a full stack engineer; I create responsive applications, solve complex problems and streamline work for clients through the software I develop. I am passionate about helping people and creating the most innovative tools for my clients in order to simplify their daily lives. I am curious, hard-working and adaptive which enables me to succeed under any condition.</p><br />
+            <p>I am a full stack engineer; I create responsive applications, solve complex problems and streamline work for clients through the software I develop. I am passionate about helping people and creating the most innovative tools for my clients in order to simplify their daily lives. I am curious, hard-working and adaptive which enables me to succeed under any condition.</p><br /><br />
             <div><Footer /></div>
         </div>
     );
