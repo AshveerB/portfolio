9f67b270597681d0eff8d6e7dae4f043d9212633
AshveerB <ashveerbhayroo@gmail.com>
2021-01-26 19:05:09 -0800

    add burger menu for navigation

---
+++ src/components/navigation/BurgerLinks.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BurgerLinks = () => {
+    return (
+        <div>
+            <div className='burgerLinks'>
+				<Link to='/home' className='hvr-grow white'>
+					Home
+				</Link>
+				<Link to='/about' className='hvr-grow white'>
+					About
+				</Link>
+				<Link to='/projects' className='hvr-grow white'>
+					Projects
+				</Link>
+			</div>
+        </div>
+    );
+};
+
+export default BurgerLinks;
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import './Navigation.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import BurgerLinks from './BurgerLinks';
+import './Navigation.css';
 
 const Navigation = () => {
-    return (
-        <nav className='navigationContainer'>
-            <div className='brand'>Ashveer Bhayroo<br />Software Engineer</div>
-            <div className='navigation'>
-                <span><Link to='/home' className='hvr-grow white'>Home</Link></span>
-                <span><Link to='/about' className='hvr-grow white'>About</Link></span>
-                <span><Link to='/projects' className='hvr-grow white'>Projects</Link></span>
-            </div>
-        </nav>
-    );
+	const [menuDisplay, setMenuDisplay] = useState(false);
+	const menuClick = () => {
+		setMenuDisplay(!menuDisplay);
+	};
+	return (
+		<div>
+			<nav className='navigationContainer'>
+				<div className='brand'>
+					Ashveer Bhayroo
+					<br />
+					Software Engineer
+				</div>
+				<div className='navigation'>
+					<span className='links'>
+						<Link to='/home' className='hvr-grow white'>
+							Home
+						</Link>
+					</span>
+					<span className='links'>
+						<Link to='/about' className='hvr-grow white'>
+							About
+						</Link>
+					</span>
+					<span className='links'>
+						<Link to='/projects' className='hvr-grow white'>
+							Projects
+						</Link>
+					</span>
+				</div>
+				<div className='burgerContainer'>
+					<div className='hamburgerNavigation hvr-grow' onClick={menuClick}>
+						<span className='burger'></span>
+						<span className='burger'></span>
+						<span className='burger'></span>
+					</div>
+				</div>
+			</nav>
+			<div>{ menuDisplay ? <BurgerLinks /> : null }</div>
+		</div>
+	);
 };
 
 export default Navigation;
